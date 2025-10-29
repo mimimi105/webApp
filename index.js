7357993a16baadada9bc042df27722d33185b574
衣笠みのる <168079463+minoru-kinugasa-105@users.noreply.github.com>
2025-10-29 15:28:31 +0900

    feat: bundle-build用のpackageを追加

---
+++ index.js
@@ -0,0 +1,13 @@
+import jwt from "jwt-simple";
+import crypto from "crypto-browserify";
+import { Transform } from "stream-browserify";
+import { Buffer } from "buffer";
+
+window.Buffer = Buffer;
+window.crypto = crypto;
+window.Transform = Transform;
+
+window.JWT = {
+  encode: jwt.encode,
+  decode: jwt.decode
+};
